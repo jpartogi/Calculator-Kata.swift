d42b0c36bc92dfa33c335a9bddce6b6af02f254d
Joshua Partogi <jpartogi@lianas-imac.lan>
2017-03-24 21:26:33 +0800

    Initial Commit

---
+++ Calculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calculator
+//
+//  Created by Joshua Partogi on 24/3/17.
+//  Copyright © 2017 Joshua Partogi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
